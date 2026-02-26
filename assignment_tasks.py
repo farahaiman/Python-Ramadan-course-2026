5487afd9ac94707b7ef95f942af9673308e295e9
farahaiman <farahaiman548@gmail.com>
2026-02-26 16:49:34 +0500

    Create assignment_tasks.py

---
+++ assignment_tasks.py
@@ -0,0 +1,38 @@
+#print("....Twinkle Twinkle poem print karna....")
+
+#print("Twinkle, twinkle, little star,")
+#print("    How I wonder what you are!")
+#print("        Up above the world so high,")
+#print("        Like a diamond in the sky.")
+#print("Twinkle, twinkle, little star,")
+#print("    How I wonder what you are")
+
+#print("....Python version check karna....")
+
+#import sys
+#print("Python version:")
+#print(sys.version)
+
+#print("....Current date and time...")
+
+#import datetime
+#now = datetime.datetime.now()
+#print("Current date and time:", now)
+
+#print("...Circle ka area (radius input)...")
+#import math
+#radius = float(input("Enter radius of circle: "))
+#area = math.pi * radius**2
+#print("Area of circle:", area)
+
+#First aur Last name reverse order
+#first = input("Enter first name: ")
+#last = input("Enter last name: ")
+#print(last, first)
+
+#two numbers addition
+#num1 = float(input("Enter first number: "))
+#num2 = float(input("Enter second number: "))
+#print("Addition:", num1 + num2)
+
+
