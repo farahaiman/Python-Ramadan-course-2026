dec17bfac4c1149858cbabbd34dcfb94043d6997
farahaiman <farahaiman548@gmail.com>
2026-02-28 20:03:15 +0500

    Create List.py

---
+++ List.py
@@ -0,0 +1,22 @@
+#List ki length print karna
+#my_list = [10, 20, 30, 40, 50]
+#print("Length of list:", len(my_list))
+
+#List ke numeric items ka sum
+#my_list = [10, 20, 30, 40, 50]
+#print("Sum of list items:", sum(my_list))
+
+#List ka sabse bada number
+#my_list = [10, 20, 30, 40, 50]
+#print("Largest number in list:", max(my_list))
+
+#Elements jo 5 se chhote hain
+
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+#print("Numbers less than 5:")
+#for num in a:
+    #if num < 5:
+    #    print(num)
+
+
