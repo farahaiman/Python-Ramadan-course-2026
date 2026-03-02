585ce676112c934c2eb1559bf3d778938db29937
farahaiman <farahaiman548@gmail.com>
2026-03-02 20:38:58 +0500

    Create DictionaryAndLoop.py

---
+++ DictionaryAndLoop.py
@@ -0,0 +1,57 @@
+#Check if list mein numeric value hai (for loop se)
+#my_list = ["apple", "banana", 10, "cat"]
+
+#found = False
+#for item in my_list:
+    #if isinstance(item, (int, float)):   # check agar number hai
+        #found = True
+       # break
+
+#if found:
+   # print("List mein numeric value hai")
+#else:
+   # print("List mein koi numeric value nahi hai")
+
+
+#2.Dictionary mein ek key add karna
+#my_dict = {"name": "Farah", "age": 20}
+#print("Before:", my_dict)
+
+#my_dict["city"] = "Karachi"   # new key add
+#print("After:", my_dict)
+
+#3.Dictionary ke numeric items ka sum
+#my_dict = {"a": 10, "b": 20, "c": "hello", "d": 30}
+
+#total = 0
+#for value in my_dict.values():
+   # if isinstance(value, (int, float)):
+      #  total += value
+
+#print("Sum of numeric values:", total)
+
+
+#5. List ke duplicate values identify karna
+
+#my_list = [1, 2, 3, 2, 4, 5, 1, 6]
+
+#duplicates = []
+#for item in my_list:
+   # if my_list.count(item) > 1 and item not in duplicates:
+    #    duplicates.append(item)
+
+#print("Duplicate values:", duplicates)
+
+#Check if given key dictionary mein exist karti hai
+#my_dict = {"name": "Ali", "age": 20, "city": "Karachi"}
+
+#key = input("Enter key to check: ")
+
+#if key in my_dict:
+  #  print("Key exists in dictionary")
+#else:
+  #  print("Key does not exist")
+#
+
+
+
